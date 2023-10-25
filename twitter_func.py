a98008b262c507ca8127b1dc5d639666c87cdc29
raska0010 <raska0010@gmail.com>
2023-10-25 21:19:08 +0200

    added twitter_func with get_kaggle function

---
+++ twitter_func.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from kaggle.api.kaggle_api_extended import KaggleApi
+import zipfile
+import os
+
+
+
+# downloads a kaggle dataset and unzips it
+# url = 'url of the dataset', file_name='file name of the dataset, see on url', path='optional
+
+def get_kaggle(url, file_name, path='data/'):
+
+    load_dotenv()
+
+    if os.path.isfile('data/' + file_name):
+        print('The file already exists')
+        return
+
+    api = KaggleApi()
+    api.authenticate()
+
+    url = url.removeprefix('https://www.kaggle.com/datasets/')
+
+    api.dataset_download_file(url, file_name, path=path)
+
+    file_name = zipfile.ZipFile(path + file_name + '.zip', 'r')
+    file_name.extractall('data')
+
